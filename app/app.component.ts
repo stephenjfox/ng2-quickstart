f09a5d959f779db16a2f49c4a16a0bce21fe4573
Stephen Fox <stevethecoder34@gmail.com>
2016-05-19 12:37:17 -0600

    Part 4 completed: Added Service abstraction layer, mocking of Hero objects, and asynchronous support

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 import { HeroDetailComponent } from './hero-detail.component';
 
 @Component({
@@ -65,28 +66,26 @@ import { HeroDetailComponent } from './hero-detail.component';
     border-radius: 4px 0 0 4px;
   }
 `],
-  directives: [HeroDetailComponent]
+  directives: [HeroDetailComponent],
+  providers: [HeroService]
 
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes'
   selectedHero: Hero
-  public heroes = HEROES
+  public heroes: Hero[]
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes()
+  }
 
   onSelect(hero: Hero) {
     this.selectedHero = hero
   }
-}
 
-let HEROES: Hero[] = [
-  { "id": 11, "name": "Mr. Nice" },
-  { "id": 12, "name": "Narco" },
-  { "id": 13, "name": "Bombasto" },
-  { "id": 14, "name": "Celeritas" },
-  { "id": 15, "name": "Magneta" },
-  { "id": 16, "name": "RubberMan" },
-  { "id": 17, "name": "Dynama" },
-  { "id": 18, "name": "Dr IQ" },
-  { "id": 19, "name": "Magma" },
-  { "id": 20, "name": "Tornado" }
-]
+  getHeroes() {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes)
+  }
+}
