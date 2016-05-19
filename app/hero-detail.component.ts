f6d474a9d69584178bc291a3b9bf15ebff684491
Stephen Fox <stevethecoder34@gmail.com>
2016-05-19 13:30:22 -0600

    Updated the routes, but deep linking doesn't work yet

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,12 +1,17 @@
-import { Component, Input } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { RouteParams } from '@angular/router-deprecated'
+
 import { Hero } from './hero';
+import { HeroService } from './hero.service'
 
 @Component({
   selector: 'my-hero-detail',
   template: `
   <div *ngIf="hero">
     <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>id: </label>{{hero.id}}
+    </div>
     <div>
       <label>name: </label>
       <input [(ngModel)]="hero.name" placeholder="name"/>
@@ -14,7 +19,18 @@ import { Hero } from './hero';
   </div>
 `
 })
-export class  HeroDetailComponent {
-  @Input()
+export class  HeroDetailComponent implements OnInit {
   hero: Hero
+
+  constructor(
+    private heroService: HeroService,
+    private routeParams: RouteParams) {
+    // body goes here
+  }
+
+  ngOnInit() {
+    let id = +this.routeParams.get('id');
+    this.heroService.getHero(id)
+      .then(hero => this.hero = hero)
+  }
 }
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -5,6 +5,18 @@ import { HEROES } from './mock-heroes'
 
 @Injectable()
 export class HeroService {
+
+  getHero(id: number) {
+    // couldn't we filter beforehand, with something like:
+    /*
+    return Promise.resolve(HEROES.filter(hero => hero.id === id))
+    */
+    // but that would be coupled to our mock... I see what you did there, Google
+    return Promise.resolve(HEROES).then(
+      heroes => heroes.filter(hero => hero.id === id)[0]
+    )
+  }
+
   getHeroes() {
     // get 'em
     return Promise.resolve(HEROES)
